146076369bc1120c108acbe8cc4a9915ac5c2302
stephanieodasing <stephanieodasing@gmail.com>
2021-05-18 15:26:25 +0800

    connected API and React

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component, Fragment} from "react";
+import {
+  CssBaseline,
+  withStyles,
+} from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import AppHeader from './views/components/Header';
+import Home from './views/components/Home';
+
+
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { apiResponse: "" };
+  // }
+  // callAPI() {
+  //   fetch("http://localhost:9000/testAPI")
+  //       .then(res => res.text())
+  //       .then(res => this.setState({ apiResponse: res }));
+  // }
+  
+  // componentWillMount() {
+  //   this.callAPI();
+  // }
+
+
+
+  
+  const styles = theme => ({
+    main: {
+      padding: theme.spacing(3),
+      [theme.breakpoints.down('xs')]: {
+        padding: theme.spacing(2),
+      },
+    },
+  });
+  
+  const App = ({ classes }) => (
+    <Fragment>
+      <CssBaseline />
+      <AppHeader />
+      <main className={classes.main}>
+        <Home />
+      </main>
+    </Fragment>
   );
-}
-
-export default App;
+  
+  export default withStyles(styles)(App);
